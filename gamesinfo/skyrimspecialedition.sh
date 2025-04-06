27bd44da5712f32c57bde458dffa8cc7b2746266
lying_cak3 <zeo4500@gmail.com>
2025-04-06 15:49:10 -0700

    Revert skse change

--- gamesinfo/skyrimspecialedition.sh
+++ gamesinfo/skyrimspecialedition.sh
@@ -3,10 +3,11 @@ game_nexusid="skyrimspecialedition"
 game_appid=489830
 game_executable="SkyrimSELauncher.exe"
 game_protontricks=("xaudio2_7=native" "vcrun2022")
-game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_00_20.7z"
+game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_02_06.7z"
 game_scriptextender_files=( \
 	"skse64_2_00_20/Data" \
-	"skse64_2_00_20/skse64_1_5_97.dll" \
+	"skse64_2_00_20/skse64_1_6_1170.dll" \
 	"skse64_2_00_20/skse64_loader.exe" \
+	"skse64_2_00_20/skse64_steam_loader.dll" \
 )
 
